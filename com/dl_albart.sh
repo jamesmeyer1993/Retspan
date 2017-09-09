e1ea34e13919864905908c2df98bc9fde61c0fe9
unawarecitizen <jmeyer1993@outlook.com>
2017-09-09 17:53:47 -0500

    Removed extra strings

--- com/dl_albart.sh
+++ com/dl_albart.sh
@@ -3,5 +3,5 @@
 url_AlbumArt=$("youtube-dl --playlist-items 1 --get-thumbnail $url")
 $(wget $url_AlbumArt)
 
-#youtube-dl --list-thumbnails https://www.youtube.com/playlist?list=PLPiidWAbJJA7YYFABxoACWZOgUbQpHuA2
+#youtube-dl --list-thumbnails "url goes here"
 # wget ^
